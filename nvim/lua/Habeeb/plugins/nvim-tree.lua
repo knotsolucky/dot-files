43d02d990950a7a0e1a748a19d9f8a783c5e4793
Habeeb Giwa <habeebgiwa@Habeebs-MacBook-Air.local>
2024-11-11 01:00:30 +0000

    Added even more

--- nvim/lua/Habeeb/plugins/nvim-tree.lua
+++ nvim/lua/Habeeb/plugins/nvim-tree.lua
@@ -1,71 +1,74 @@
 return {
-  "nvim-tree/nvim-tree.lua",
-  dependencies = "nvim-tree/nvim-web-devicons",
-  config = function()
-    local nvimtree = require("nvim-tree")
+	"nvim-tree/nvim-tree.lua",
+	dependencies = {
+		"nvim-tree/nvim-web-devicons",
+		"folke/which-key.nvim",
+	},
+	config = function()
+		local nvimtree = require("nvim-tree")
+		local wk = require("which-key")
 
-    -- recommended settings from nvim-tree documentation
-    vim.g.loaded_netrw = 1
-    vim.g.loaded_netrwPlugin = 1
+		-- recommended settings from nvim-tree documentation
+		vim.g.loaded_netrw = 1
+		vim.g.loaded_netrwPlugin = 1
 
-    nvimtree.setup({
-      view = {
-        width = 35,
-        relativenumber = false,
-      },
-      -- change folder arrow icons
-      renderer = {
-        indent_markers = {
-          enable = true,
-        },
-        icons = {
-          glyphs = {
-            folder = {
-              arrow_closed = "", -- arrow when folder is closed
-              arrow_open = "", -- arrow when folder is open
-            },
-          },
-        },
-      },
-      -- disable window_picker for
-      -- explorer to work well with
-      -- window splits
-      actions = {
-        open_file = {
-          window_picker = {
-            enable = false,
-          },
-        },
-      },
-      filters = {
-        custom = { ".DS_Store" },
-      },
-      git = {
-        ignore = false,
-      },
-    })
+		-- Setup nvim-tree
+		nvimtree.setup({
+			view = {
+				width = 35,
+				relativenumber = false,
+			},
+			renderer = {
+				indent_markers = {
+					enable = true,
+				},
+				icons = {
+					glyphs = {
+						folder = {
+							arrow_closed = "",
+							arrow_open = "",
+						},
+					},
+				},
+			},
+			actions = {
+				open_file = {
+					window_picker = {
+						enable = false,
+					},
+				},
+			},
+			filters = {
+				custom = { ".DS_Store" },
+			},
+			git = {
+				ignore = false,
+			},
+		})
 
-    -- set keymaps
-    local keymap = vim.keymap -- for conciseness
-
-    -- Toggle file explorer
-    keymap.set("n", "<leader>ee", "<cmd>NvimTreeToggle<CR>", { desc = "Toggle file explorer" })
-    keymap.set("n", "<leader>ef", "<cmd>NvimTreeFocus<CR>", { desc = "Focus on file explorer" })
-    keymap.set("n", "<leader>er", "<cmd>NvimTreeRefresh<CR>", { desc = "Refresh file explorer" })
-
-    -- Switch between nvim-tree and editor
-    keymap.set("n", "<leader>es", function()
-      local view = require("nvim-tree.view")
-      if view.is_visible() then
-        -- Check if the current window is nvim-tree, if so, switch to the next window
-        if vim.fn.winnr() == view.get_winnr() then
-          vim.cmd("wincmd w")      -- Switch to the next window
-        else
-          vim.cmd("NvimTreeFocus") -- Focus on nvim-tree
-        end
-      else
-        vim.cmd("NvimTreeToggle") -- If nvim-tree is not open, toggle it
-      end
-    end, { desc = "Switch between file explorer and editor" })
-  end
+		-- Define keymaps using which-key's updated syntax
+		wk.register({
+			e = {
+				name = "   Explorer", -- Added folder icon next to Explorer
+				e = { "<cmd>NvimTreeToggle<CR>", "Toggle file explorer" },
+				f = { "<cmd>NvimTreeFocus<CR>", "Focus on file explorer" },
+				r = { "<cmd>NvimTreeRefresh<CR>", "Refresh file explorer" },
+				s = {
+					function()
+						local view = require("nvim-tree.view")
+						if view.is_visible() then
+							if vim.fn.winnr() == view.get_winnr() then
+								vim.cmd("wincmd w")
+							else
+								vim.cmd("NvimTreeFocus")
+							end
+						else
+							vim.cmd("NvimTreeToggle")
+						end
+					end,
+					"Switch between file explorer and editor",
+				},
+			},
+		}, { prefix = "<leader>" })
+	end,
 }
