cb8fc76ffeec0bc4970f04bbe8b20b992d1820d6
kamicody1010 <olamatandagiwa@gmail.com>
2024-12-20 06:17:10 +0000

    Updated dotfiles
    * Updated Alacritty from using Catppuccin to Tokyo Nights
    * Updated Neovim from using Catppuccin theme to Tokyo Nights

--- nvim/lua/Habeeb/plugins/colorscheme.lua
+++ nvim/lua/Habeeb/plugins/colorscheme.lua
@@ -1,11 +1,10 @@
 return {
-  {
-    "catppuccin/nvim",
-    name = "catppuccin",
-    priority = 1000,
-    config = function()
-      vim.cmd.colorscheme("catppuccin-mocha") -- Apply the Catppuccin theme in Mocha flavor
-    end,
-  },
+  "folke/tokyonight.nvim",
+  lazy = false,
+  priority = 1000,
+  opts = {},
+  config = function()
+    vim.cmd("colorscheme tokyonight")
+  end,
 }
 
