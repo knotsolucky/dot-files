30fee714c7917dcc873cf024c031c368dee930c3
knotsolucky <2003habeeb@gmail.com>
2025-08-29 14:11:08 +0100

    Shell Scripts

---
+++ mac-install.sh
@@ -0,0 +1,66 @@
+brew tap homebrew/autoupdate
+brew tap homebrew/bundle
+brew tap homebrew/cask-fonts
+brew tap homebrew/services
+
+brew install wget
+brew install npm
+brew install git
+brew install zsh-autosuggestions
+brew install zsh-syntax-highlighting
+brew install yazi
+brew install eza
+brew install zoxide
+brew install neovim
+brew install tmux
+brew install starship
+brew install assh
+brew install fastfetch
+brew install zip
+brew install unzip
+brew install stow
+brew install lazygit
+brew install tpm
+brew install bash
+brew install bat
+brew install docker
+brew install gcc
+brew install ffmpeg
+brew install go
+brew install mysql
+brew install mysql@8.0
+brew install neofetch
+brew install node
+brew install openjdk
+brew install openvpn
+brew install putty
+brew install python@3.11
+brew install rust
+brew install tree
+brew install luarocks
+brew install lua@5.1
+brew install btop
+brew install clang
+brew install lazydocker
+brew install fzf
+brew install gh
+brew install gitui
+brew install vim-plug
+
+brew install --cask arc
+brew install --cask brave-browser
+brew install --cask discord
+brew install --cask font-geist-mono-nerd-font
+brew install --cask font-jetbrains-mono-nerd-font
+brew install --cask obsidian
+brew install --cask unnaturalscrollwheels
+brew install --cask visual-studio-code
+brew install --cask whatsapp
+brew install --cask raycast
+brew install --cask 1password
+brew install --cask craft
+brew install --cask setapp
+brew install --cask tidal
+brew install --cask ghostty
+brew install --cask alt-tab
+
