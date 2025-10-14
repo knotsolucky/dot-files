fa8fd6403d9e213f1b94a2c7f09f13efa53b5046
Habeeb Giwa <habeebgiwa@Habeebs-MacBook-Pro.local>
2025-10-14 01:13:36 +0100

    Starting Neovim From Scratch

---
+++ nvim/lua/core/options.lua
@@ -0,0 +1,15 @@
+local opt = vim.opt
+
+opt.number = true
+opt.relativenumber = true
+opt.tabstop = 2
+opt.expandtab = true
+opt.shiftwidth = 2
+opt.autoindent = true
+opt.smartcase = true
+opt.termguicolors = true -- Allows for colourshemes to properly show up
+opt.background = "dark"
+opt.signcolumn = "yes"
+
+
+opt.clipboard:append("unnamedplus")
